1ee638606dd147d3faf3938c87aa7770beaf41dd
RicardoValus <ricardovalus@gmail.com>
2024-06-10 00:02:32 -0300

    Small adjusments and upload APK

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'RockersCut',
+  appName: 'Rockers',
   webDir: 'www',
   server: {
     androidScheme: 'https'
